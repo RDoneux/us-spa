d4aa17bb3e0dcf5c37b6df23a657dff1b70e48e7
Rob Doneux <robertdoneux@gmail.com>
2024-12-07 20:12:09 +0000

    Add image service URL to Home component

--- src/routes/home/Home.tsx
+++ src/routes/home/Home.tsx
@@ -1,9 +1,12 @@
 import styles from './Home.module.css';
 
+const US_IMAGE_SERVICE_URL = process.env.US_IMAGE_SERVICE_URL;
+
 export default function Home() {
   return (
     <div className={styles['container']}>
       <h1>TPLT | WEB SPA</h1>
+      <img src={US_IMAGE_SERVICE_URL + '?filename=17dc60cd-bd8a-4268-85ef-4eff3ab77220'} />
     </div>
   );
 }
