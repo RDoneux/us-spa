136ea7a481f9b292b906e325d0bb61fbb7b1bf94
Rob Doneux <robertdoneux@gmail.com>
2024-12-08 21:34:51 +0000

    Update Home component to use GATEWAY_SERVICE for image fetching

--- src/routes/home/Home.tsx
+++ src/routes/home/Home.tsx
@@ -2,14 +2,14 @@ import { useEffect, useState } from 'react';
 import styles from './Home.module.css';
 import axios, { AxiosResponse } from 'axios';
 
-const US_IMAGE_SERVICE_URL = import.meta.env.VITE_US_IMAGE_SERVICE_URL;
+const GATEWAY_SERVICE = import.meta.env.AUTH_SERVICE;
 
 export default function Home() {
 
   const [image, setImage] = useState();
 
   useEffect(() => {
-    axios.get(US_IMAGE_SERVICE_URL + '/get?filename=33f27c10-be7c-433f-a13e-1131c2e65c27').then((response: AxiosResponse) => {
+    axios.get(GATEWAY_SERVICE + '/get?filename=33f27c10-be7c-433f-a13e-1131c2e65c27').then((response: AxiosResponse) => {
       console.log(response.data);
       setImage(response.data);
       console.log(image)
@@ -19,7 +19,7 @@ export default function Home() {
   return (
     <div className={styles['container']}>
       <h1>TPLT | WEB SPA</h1>
-      {/* <img src={US_IMAGE_SERVICE_URL + '/get?filename=33f27c10-be7c-433f-a13e-1131c2e65c27'} /> */}
+      <img src={GATEWAY_SERVICE + '/get?filename=33f27c10-be7c-433f-a13e-1131c2e65c27'} />
       
     </div>
   );
