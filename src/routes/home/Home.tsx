6f01be8b06c3bd1449263924283f6fb9594f5dbb
Rob Doneux <robertdoneux@gmail.com>
2024-12-08 21:49:39 +0000

    Refactor Home component to simplify image fetching and improve code readability

--- src/routes/home/Home.tsx
+++ src/routes/home/Home.tsx
@@ -1,26 +1,17 @@
-import { useEffect, useState } from 'react';
 import styles from './Home.module.css';
-import axios, { AxiosResponse } from 'axios';
 
 const GATEWAY_SERVICE = import.meta.env.VITE_AUTH_SERVICE;
 
 export default function Home() {
-
-  const [image, setImage] = useState();
-
-  useEffect(() => {
-    axios.get(GATEWAY_SERVICE + '/get?filename=33f27c10-be7c-433f-a13e-1131c2e65c27').then((response: AxiosResponse) => {
-      console.log(response.data);
-      setImage(response.data);
-      console.log(image)
-    })
-  }, [])
-
   return (
     <div className={styles['container']}>
       <h1>TPLT | WEB SPA</h1>
-      <img src={GATEWAY_SERVICE + '/get?filename=33f27c10-be7c-433f-a13e-1131c2e65c27'} />
-      
+      <img
+        style={{ width: '250px' }}
+        src={
+          GATEWAY_SERVICE + '/get?filename=33f27c10-be7c-433f-a13e-1131c2e65c27'
+        }
+      />
     </div>
   );
 }
