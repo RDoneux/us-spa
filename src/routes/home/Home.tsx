bde0700a1b0da3db058cd9589737f52b27743e46
Rob Doneux <robertdoneux@gmail.com>
2024-12-08 07:37:20 +0000

    Update image source URL in Home component to use 'get' endpoint

--- src/routes/home/Home.tsx
+++ src/routes/home/Home.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className={styles['container']}>
       <h1>TPLT | WEB SPA</h1>
-      <img src={US_IMAGE_SERVICE_URL + '?filename=17dc60cd-bd8a-4268-85ef-4eff3ab77220'} />
+      <img src={US_IMAGE_SERVICE_URL + 'get?filename=17dc60cd-bd8a-4268-85ef-4eff3ab77220'} />
       
     </div>
   );
