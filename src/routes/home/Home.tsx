3080f6bce056466eb776c7a03e689b816f95df18
Rob Doneux <robertdoneux@gmail.com>
2024-12-24 09:11:08 +0000

    Refactor Home component to simplify date range handling; remove generateDateRange function and adjust data fetching logic

--- src/routes/home/Home.tsx
+++ src/routes/home/Home.tsx
@@ -1,6 +1,6 @@
 import styles from './Home.module.css';
 import Timeline from '../../components/timeline/Timeline';
-import { addMonths, endOfMonth, isBefore, startOfMonth } from 'date-fns';
+import { endOfMonth, startOfMonth } from 'date-fns';
 import { useEffect, useState } from 'react';
 import { AxiosResponse } from 'axios';
 import useAxios from '../../hooks/useAxios';
@@ -12,28 +12,11 @@ export default function Home() {
   useEffect(() => {
     async function getRecordRange() {
       const result: AxiosResponse = await axios.get('/events/record-range');
-      const { startDate, endDate } = result.data;
-
-      setDateRange(generateDateRange(new Date(startDate), new Date(endDate)));
+      setDateRange(result.data.reverse());
     }
     getRecordRange();
     // eslint-disable-next-line
   }, []);
-
-  function generateDateRange(startDate: Date, endDate: Date): string[] {
-    const dates: string[] = [];
-    let currentDate = startDate;
-
-    while (
-      isBefore(currentDate, endDate) ||
-      currentDate.getMonth() === endDate.getMonth()
-    ) {
-      dates.push(currentDate.toISOString());
-      currentDate = addMonths(currentDate, 1);
-    }
-
-    return dates;
-  }
 
   return (
     <div className={styles['container']}>
